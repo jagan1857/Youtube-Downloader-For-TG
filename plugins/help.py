2d62215b5e82dcef35cc5617549bb19ecca57c47
jagan1857 <107793234+jagan1857@users.noreply.github.com>
2022-09-08 10:32:36 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -4,8 +4,8 @@ import config
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
 	alpha2 = InlineKeyboardMarkup([
-		[InlineKeyboardButton("ʀᴇᴘᴏʀᴛ ʙᴜɢs 👾", url="https://t.me/AlphaX_SUPPORT")]
+		[InlineKeyboardButton("ʀᴇᴘᴏʀᴛ ʙᴜɢs 👾", url="https://t.me/MR_JAGANMOHAN")]
                 ])
 
 	help_image = config.HPIC
-	await message.reply_photo(help_image,  caption="**💡 English HELP 📃...**\n \n__• Just Send your Youtube video url 🌟__ \n__• And i will give Method list to select your choice 😋__\n \n======================\n \n**💡 සින්හල උපදෙස් 📃...**\n \n__• කොපි කරගත් Youtube ලින්කුව එවන්න.__\n__• එවිට ලැබෙන ලැයිස්තුවෙන් අවශ්‍ය ප්‍රමාණය හා මාදිලිය තෝරාදෙන්න.__\n\n•••••••••••••••••••••••\n__• 😊 This bot is fully free.__\n`•⚙ Don't pay anyone for Bots like this.`\n\n",reply_markup=alpha2)
+	await message.reply_photo(help_image,  caption="**💡 English HELP 📃...**\n \n__• Just Send your Youtube video url 🌟__ \n__• And i will give Method list to select your choice 😋__\n \n======================\n \n**💡__ \n__•  😊 This bot is fully free.__\n`•⚙ Don't pay anyone for Bots like this.`\n\n",reply_markup=Botz hub)
